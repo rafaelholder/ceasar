3366da185033b38864617d4a87eed431a22064ba
Rafael <rafaelholder0106@gmail.com>
2023-10-27 19:46:19 -0300

    Primeiros passos

---
+++ CalcIMC.py
@@ -0,0 +1,34 @@
+import time
+
+# IMC = peso / (altura * altura)
+def calcularIMC(): 
+    weight = float(input("Digite seu peso(KG): "))  
+    height = (float(input("Digite sua altura(CM): ")) / 100)
+    imc = weight / (height ** 2)
+
+    return imc
+#Classificar IMC
+def classificarIMC(imc):
+    classe = ''
+    if imc > 1 and imc <= 18.4:
+        classe = 'Magreza'
+    elif imc >= 18.5 and imc <= 24.9:
+        classe = 'Normal'
+    elif imc >= 25 and imc <= 29.9:
+        classe = 'Sobrepeso'
+    elif imc >= 30 and imc <= 34.9:
+        classe = 'Obesidade grau I'
+    elif imc >= 35 and imc <= 39.9:
+        classe = 'Obesidade grau II'
+    elif imc >= 40:
+        classe = 'Obesidade grau III'
+    else:
+        classe = 'Invalido'
+    
+    return classe
+
+imc = calcularIMC()
+classeIMC = classificarIMC(imc)
+print('Processando...')
+time.sleep(2)
+print('\nSeu IMC eh de: {:.1f} \nSua classificacao eh: {}'.format(imc, classeIMC))
---
+++ CalcTaxaMetaBasal.py
@@ -0,0 +1,40 @@
+#CALCULADORA DE TAXA METABÓLICA BASAL
+def CalcularTaxaBasal(idade, altura, peso, sexo, fatorAtvFisica):
+    match fatorAtvFisica:
+        case 1:
+            fatorAtvFisica = 1.2
+        case 2: 
+            fatorAtvFisica = 1.375
+        case 3:
+            fatorAtvFisica = 1.55
+        case 4: 
+            fatorAtvFisica = 1.725
+        case 5:
+            fatorAtvFisica = 1.9
+
+    if sexo == 1:
+        taxaMetabolicaBasal = float(fatorAtvFisica * (66 + (13.7 * peso) + (5 * altura) - (6.8 * idade)))
+    elif sexo == 2:
+        taxaMetabolicaBasal = float(fatorAtvFisica * (655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade)))
+    
+    return taxaMetabolicaBasal
+
+#VARIÁVEIS      
+idade = int(input('Digite a sua idade: ')) 
+altura = float(input('Agora a sua altura(CM): '))
+peso = float(input('Agora o seu peso(KG): ')) 
+sexo = int(input('Finalizando, qual é o seu sexo? \n   Digite 1 para Masculino e 2 para Feminino: '))
+
+fatorAtvFisica = float(input('''\nQual é a sua taxa de atividade física?
+    1 - Sedentários (pouco ou nenhum exercício)
+    2 - Levemente ativo (exercício leve 1 a 3 dias por semana)
+    3 - Moderadamente ativo (exercício moderado, faz esportes 3 a 5 dias por semana)
+    4 - Altamente ativo (exercício pesado de 5 a 6 dias por semana)
+    5 - Extremamente ativo (exercício pesado diariamente e até 2 vezes por dia)  
+Digite sua resposta: '''))
+
+#MOSTRANDO A TAXA
+TMB= CalcularTaxaBasal(idade, altura, peso, sexo, fatorAtvFisica)
+print(f'Sua taxa metabólica basal é de: {str(TMB)}')
+
+
